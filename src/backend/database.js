74b80f4b93915fd70b3fba68b64d9639d9fd4fbe
Darya-s <darya.stepanova95@gmail.com>
2023-10-28 17:33:43 +0200

    fixed ssl requirement issue

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,20 +1,21 @@
-require("dotenv").config();
+require('dotenv').config();
 
 // create connection
-const knex = require("knex")({
-  client: "postgres",
+const knex = require('knex')({
+  client: 'postgres',
   connection: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: true,
   },
   pool: { min: 2, max: 10 },
 });
 
 // Check that the connection works
-knex.raw("SELECT VERSION()").then(() => {
+knex.raw('SELECT VERSION()').then(() => {
   console.log(`connection to db successful!`);
 });
 
